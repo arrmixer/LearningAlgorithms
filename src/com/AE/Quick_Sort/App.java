b7cdc48b57bad2481974311774e4f57f759859d8
Angel Rodriguez <arrmixer@gmail.com>
2017-05-27 10:58:51 -0400

    made some minor changes to QuickSort

--- src/com/AE/Quick_Sort/App.java
+++ src/com/AE/Quick_Sort/App.java
@@ -8,11 +8,11 @@ import java.util.Arrays;
 public class App {
     public static void main(String[] args) {
         int[]  a = {12, 81, 74, 43, 1098, 0, 8, 92, 17, 754, 912, 0, 6, 4 };
-        QuickSort.QuickSort(a);
+        QuickSort.quickSort(a);
         System.out.println(Arrays.toString(a));
 
         int[] myArray2 = {9,8,99,110,8,87,637,8,3,13,87,12,99};
-        QuickSort.QuickSort(myArray2);
+        QuickSort.quickSort(myArray2);
         for(int i = 0;i<myArray2.length;i++){
             System.out.println(myArray2[i]);
         }
--- src/com/AE/Quick_Sort/QuickSort.java
+++ src/com/AE/Quick_Sort/QuickSort.java
@@ -4,36 +4,38 @@ package com.AE.Quick_Sort;
  * Created by Angel on 5/5/17.
  */
 public class QuickSort {
-
-    public static void QuickSort(int A[]){
-        QuickSort(A, 0, A.length - 1);
+    //overide to allow only one parameter
+    public static void quickSort(int A[]){
+        quickSort(A, 0, A.length - 1);
     }
-
-    public static void QuickSort(int A[], int p, int r){
+// p is the start and r is the end of the array
+    public static void quickSort(int A[], int p, int r){
 
         if(p<r){
-           int q = Partition(A, p, r);
-            QuickSort(A, p, q - 1);
-            QuickSort(A, q + 1, r);
+           int q = partition(A, p, r); //
+            quickSort(A, p, q - 1); // sorts the left side
+            quickSort(A, q + 1, r); // sorts the right side
         }
 
 
     }
 
-    public static int Partition(int[] A, int p, int r){
-        int x = A[r];
+    public static int partition(int[] A, int p, int r){
+        int x = A[r]; // the pivot position
         int i = p - 1;
 
         for(int j = p;j<r;j++){
             if(A[j] <= x)
             {
                 i++;
+                //swapping the values
                 int temp = A[i];
                 A[i] = A[j];
                 A[j] = temp;
 
             }
         }
+
         int temp = A[i + 1];
         A[i + 1] = A[r];
         A[r] = temp;
